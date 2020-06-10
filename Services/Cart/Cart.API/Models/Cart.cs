6e9d79e50efb5d6171deb9be38be6e2948825f9e
mortenbk <morphy86@gmail.com>
2020-06-10 17:23:17 +0200

    Add project files.

---
+++ Services/Cart/Cart.API/Models/Cart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Cart.Models
+{
+    public partial class Cart
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public IList<CartItem> CartItems { get; set; }
+    }
+}
---
+++ Services/Cart/Cart.API/Models/CartItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cart.Models
+{
+    public partial class CartItem
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+        public decimal Price { get; set; }
+    }
+}
